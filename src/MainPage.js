72170e019952dfa57b70f09938666130156a8da6
Parkminseo” git config --global user.email 20251967@sungshin.ac.kr git config --global user.name Parkminseo” <20251967@sungshin.ac.kr>
2026-05-10 11:27:51 +0900

    진짜 최종 초기 세팅

---
+++ src/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ChatPage = () => {
+  return (
+    <div>
+      <h1>AI 챗봇 상담</h1>
+      {/* 여기에 민지가 조사한 챗봇 UI 코딩 */}
+    </div>
+  );
+};
+
+export default ChatPage;
